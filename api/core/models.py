99e1e3066118147b7b8537bfff1cb144c2eeddd2
kishik <kishik.maxim@gmail.com>
2022-11-18 23:32:57 +0300

    models

--- api/core/models.py
+++ api/core/models.py
@@ -1,15 +1,40 @@
 from django.db import models
-from django.conf import settings
-from django.utils import timezone
 from django.contrib.auth.models import User
+import requests as requests_lib
+from django.urls import reverse
 
-class Data(models.Model):
-    """Комментарии"""
-    name = models.CharField(max_length=200, blank=True, null=True)
-    description = models.CharField(max_length=200, blank=True, null=True)
-    img = models.ImageField(upload_to="core/")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='data_user', default=1)
-    time = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return str(self.id)
+class Profile(models.Model):
+    """Дополнительные данные для профиля юзера"""
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    second_name = models.CharField(max_length=100, blank=True, null=True)
+    third_name = models.CharField(max_length=100, blank=True, null=True)
+    avatar = models.ImageField(upload_to='bank/profile', default='bank/profile/avatar_default.png')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile_user', db_index=True)
+    active = models.BooleanField(default=True)
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+
+
+class CurrencyCourse(models.Model):
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='currency')
+    course_cb = models.FloatField(blank=True, default=0)
+    date = models.DateField(db_index=True)
+
+
+class Account(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile_user', db_index=True)
+    currency = models.ForeignKey(Currency, on_delete=models.PROTECT, related_name='currency')
+    value = models.FloatField(blank=True, default=0)
+    active = models.BooleanField(default=True)
+
+
+class Transfer(models.Model):
+    from_account = models.ForeignKey(Currency, on_delete=models.PROTECT, related_name='from_account')
+    to_account = models.ForeignKey(Currency, on_delete=models.PROTECT, related_name='to_account')
+    value = models.FloatField(blank=True, default=0)
+    date = models.DateTimeField()
