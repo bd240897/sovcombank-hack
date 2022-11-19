bbb77fba909a10e6fdf297ab4e407882460092a1
root <r.ilia@innopolis.university>
2022-11-19 13:27:44 +0300

    исправил функцию get_cur, добавил def:перевод валюты

---
+++ api/get_currency/convet_currency.py
@@ -0,0 +1,21 @@
+import requests
+
+def convert_currency(from_currency, amount ,to_currency): # имена валют
+  
+  #from_currency = "USD"
+  #amount = "2509"
+  #to_currency = "RUB"
+
+  url = "https://api.apilayer.com/exchangerates_data/convert?to=" + to_currency + "&from=" + from_currency + "&amount=" + amount
+
+  payload = {}
+  headers= {
+    "apikey": "JJNdsqUntjo5Q5yjQNAOR353F7D6rlxJ"
+  }
+
+  response = requests.request("GET", url, headers=headers, data = payload)
+
+  status_code = response.status_code
+  result = response.text
+
+  return result # курс валюты
