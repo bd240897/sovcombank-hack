88d37bea4718cc78035399be7a02b3e0a68b637c
root <r.ilia@innopolis.university>
2022-11-19 13:09:30 +0300

    сделал функцию get-currency

--- api/get_currency/get_current.py
+++ api/get_currency/get_current.py
@@ -1,18 +1,21 @@
 import requests
 
-display_currency = "RUB"
-base_currency = "EUR"
+def get_currency(display_currency,base_currency): # имена валют
 
-url = "https://api.apilayer.com/exchangerates_data/latest?symbols=" + display_currency + "&base=" + base_currency
 
-payload = {}
-headers= {
-  "apikey": "<api-key>"
-}
+  #display_currency = "RUB"
+  #base_currency = "EUR"
 
-response = requests.request("GET", url, headers=headers, data = payload)
+  url = "https://api.apilayer.com/exchangerates_data/latest?symbols=" + display_currency + "&base=" + base_currency
 
-status_code = response.status_code
-result = response.text
+  payload = {}  
+  headers= {
+    "apikey": "<api-key>"
+  }
 
-print(url)
+  response = requests.request("GET", url, headers=headers, data = payload)
+
+  status_code = response.status_code
+  result = response.text
+
+  return result # курс валюты
